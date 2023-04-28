463c49edcfecaf24e266a618ea1aeb3a8759a05f
rjsehrlich <racheljsehrlich@gmail.com>
2023-04-28 15:15:31 -0700

    setup starting movie controller endpoints

--- src/main/java/com/moviequote/project/controller/MovieController.java
+++ src/main/java/com/moviequote/project/controller/MovieController.java
@@ -1,11 +1,38 @@
 package com.moviequote.project.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
-@RequestMapping(path = "/api")
+@RequestMapping(path = "/api") // http://localhost:9893/api
 public class MovieController {
+    // http://localhost:9893/api/movies/
+    @GetMapping(path = "/movies/")
+    public String getMovies() {
+        return "get all movies";
+    }
 
+    // http://localhost:9893/api/movies/1
+    @GetMapping(path = "/movies/{movieId}")
+    public String getMovie(@PathVariable Long movieId) {
+        return "getting the movie with the id of " + movieId;
+    }
 
+    // http://localhost:9893/api/movies/
+    @PostMapping(path = "/movies/")
+    public String createMovie(@RequestBody String body) {
+        return "Creating a movie " + body;
+    }
+
+    // http://localhost:9893/api/movies/1
+    @PutMapping(path = "/movies/{movieId}")
+    public String updateMovie(@PathVariable Long movieId, @RequestBody String body) {
+        return "updating the movie with the id of " + movieId + " with this info " + body;
+    }
+
+    // http://localhost:9893/api/movies/1
+    @DeleteMapping(path = "/movies/{movieId}")
+    public String deleteMovie(@PathVariable String movieId) {
+        return "deleting the movie that has the id of " + movieId;
+    }
 }

b2443fdad7bafee332ba6b29fd116dfa7ed66bf4
rjsehrlich <racheljsehrlich@gmail.com>
2023-04-30 16:25:19 -0700

    initialized standard User class with instance variables and methods

---
+++ src/main/java/com/moviequote/project/model/User.java
@@ -0,0 +1,60 @@
+package com.moviequote.project.model;
+
+public class User {
+    private Long id;
+    private String userName;
+    private String emailAddress;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String userName, String emailAddress, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}

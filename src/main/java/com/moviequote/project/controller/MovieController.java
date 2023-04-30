df218f3aeb1bc1c2ffaefb48e289f04fd96a1e9c
rjsehrlich <racheljsehrlich@gmail.com>
2023-04-30 13:49:39 -0700

    setup endpoints for movieQuote operations

--- src/main/java/com/moviequote/project/controller/MovieController.java
+++ src/main/java/com/moviequote/project/controller/MovieController.java
@@ -1,10 +1,13 @@
 package com.moviequote.project.controller;
 
 import com.moviequote.project.model.Movie;
+import com.moviequote.project.model.Quote;
 import com.moviequote.project.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -50,4 +53,33 @@ public class MovieController {
         return movieService.deleteMovie(movieId);
     }
 
+    // http://localhost:9893/api/movies/1/quotes
+    @GetMapping(path = "/movies/{movieId}/quotes")
+    public List<Quote> getMovieQuotes(@PathVariable Long movieId) {
+        return ;
+    }
+
+    // http://localhost:9893/api/movies/1/quotes/1
+    @GetMapping(path = "/movies/{movieId}/quotes/{quoteId}")
+    public Quote getMovieQuote(@PathVariable Long movieId, @PathVariable Long quoteId) {
+        return ;
+    }
+
+    // http://localhost:9893/api/movies/1/quotes
+    @PostMapping(path = "/movies/{movieId}/quotes")
+    public Quote createMovieQuote(@PathVariable Long movieId, @RequestBody Quote quoteObject) {
+        return ;
+    }
+
+    // http://localhost:9893/api/movies/1/quotes/1
+    @PutMapping(path = "/movies/{movieId}/quotes/{quoteId}")
+    public Quote updateMovieQuote(@PathVariable Long movieId, @PathVariable Long quoteId, @RequestBody Quote quoteObject) {
+        return ;
+    }
+
+    // http://localhost:9893/api/movies/1/quotes/1
+    @DeleteMapping(path = "/movies/{movieId}/quotes/{quoteId}")
+    public ResponseEntity<HashMap<String, String>> deleteMovieQuote(@PathVariable Long movieId, @PathVariable Long quoteId) {
+        return ;
+    }
 }

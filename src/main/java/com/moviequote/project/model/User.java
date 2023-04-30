c73771b7800b8dad7e7218fcd48b57863f60c5e7
rjsehrlich <racheljsehrlich@gmail.com>
2023-04-30 16:27:28 -0700

    updates class to entity table

--- src/main/java/com/moviequote/project/model/User.java
+++ src/main/java/com/moviequote/project/model/User.java
@@ -1,9 +1,26 @@
 package com.moviequote.project.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String userName;
+
+    @Column(unique = true)
     private String emailAddress;
+
+    @Column
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     public User() {

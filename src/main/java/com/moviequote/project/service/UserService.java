3063421fa0765c1ad32ba6a35c7bdb746a87c5ea
rjsehrlich <racheljsehrlich@gmail.com>
2023-04-30 17:56:14 -0700

    updated UserService service to complete login process for authentication and security changes

--- src/main/java/com/moviequote/project/service/UserService.java
+++ src/main/java/com/moviequote/project/service/UserService.java
@@ -2,9 +2,18 @@ package com.moviequote.project.service;
 
 import com.moviequote.project.exception.InformationExistException;
 import com.moviequote.project.model.User;
+import com.moviequote.project.model.request.LoginRequest;
+import com.moviequote.project.model.response.LoginResponse;
 import com.moviequote.project.repository.UserRepository;
+import com.moviequote.project.security.JWTUtils;
+import com.moviequote.project.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,11 +24,19 @@ public class UserService {
     //encrypt password/ salt it
     //comes from Springboot security dependency
     private PasswordEncoder passwordEncoder;
+    private final JWTUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
+    private MyUserDetails myUserDetails;
 
     @Autowired
-    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder,
+                       JWTUtils jwtUtils, @Lazy AuthenticationManager authenticationManager,
+                       @Lazy MyUserDetails myUserDetails) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
+        this.myUserDetails = myUserDetails;
     }
 
     /**
@@ -40,5 +57,19 @@ public class UserService {
 
     public User findUserByEmailAddress(String email) {
         return userRepository.findUserByEmailAddress(email);
+    }
+
+    public ResponseEntity<?> loginUser(LoginRequest loginRequest) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword());
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            myUserDetails = (MyUserDetails) authentication.getPrincipal();
+            final String JWT = jwtUtils.generateJwtToken(myUserDetails);
+            return ResponseEntity.ok(new LoginResponse(JWT));
+        } catch (Exception e) {
+            return ResponseEntity.ok(new LoginResponse("Error : user name or password is incorrect"));
+        }
     }
 }

29d5954b54d040a950f6a2012be9cfd86cf48d2e
rjsehrlich <racheljsehrlich@gmail.com>
2023-04-30 17:18:59 -0700

    creating a UserService for handling user password saving in db with encoding

--- src/main/java/com/moviequote/project/service/UserService.java
+++ src/main/java/com/moviequote/project/service/UserService.java
@@ -1,4 +1,44 @@
 package com.moviequote.project.service;
 
+import com.moviequote.project.exception.InformationExistException;
+import com.moviequote.project.model.User;
+import com.moviequote.project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private UserRepository userRepository;
+
+    //encrypt password/ salt it
+    //comes from Springboot security dependency
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    /**
+     * createUser will accept our
+     * @param userObject
+     * if no user exists by the user email address,
+     * @return create the new user and encrypt their password using encoder
+     */
+    public User createrUser(User userObject) {
+        if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else {
+            throw new InformationExistException("user with email address " + userObject.getEmailAddress() +
+                    " already exists");
+        }
+    }
+
+    public User findUserByEmailAddress(String email) {
+        return userRepository.findUserByEmailAddress(email);
+    }
 }
